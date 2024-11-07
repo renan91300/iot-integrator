5963b17f54276ec6141e7ad69bf140d7bf7ac14e
Renan Gomes Poggian <renanpoggiangomes@gmail.com>
2024-11-07 15:54:06 -0300

    ⚰️ remove department model

--- api/admin.py
+++ api/admin.py
@@ -3,13 +3,11 @@ from django.contrib import admin
 from accounts.models import UserAccount
 
 from api.models.category import Category
-from api.models.department import DepartmentModel
 from api.models.location import LocationModel
 from api.models.log import LogModel
 from api.models.device import DeviceModel
 
 admin.site.register(Category)
-admin.site.register(DepartmentModel)
 admin.site.register(LocationModel)
 admin.site.register(LogModel)
 admin.site.register(DeviceModel)
--- api/serializers/department.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from api.models.department import DepartmentModel
-
-
-class DepartmentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DepartmentModel
-        fields = "__all__"
